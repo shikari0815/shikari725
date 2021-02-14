9d092d286d31561ab14124d2f884fd67a8a970dc
shikari0815 <zm19113@josai.ac.jp>
2021-02-14 11:48:31 +0000

    0214

--- ex.1.js
+++ ex.1.js
@@ -1,1 +1,3 @@
-function sayhello1() {alert('ゴルフ');}
+function txtchange1() {document.getElementById('txt1').innerHTML = 'はずれ';}
+function txtchange２() {document.getElementById('txt1').innerHTML = '当たり';}
+function txtchange３() {document.getElementById('txt1').innerHTML = 'はずれ';}
